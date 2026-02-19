1e9d9f524fd5b11ab3dc13d8df000588d1fa7513
Christian Kohlschütter <christian@kohlschutter.com>
2026-02-19 12:11:04 +0000

    Add setup_var.efi

---
+++ build-setup_var.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(dirname $0)
+srcDir=$(pwd)/setup_var.efi
+targetDir=$(pwd)/target/setup_var
+mkdir -p "${targetDir}"
+
+export RUSTUP_HOME="${targetDir}/rustup"
+export CARGO_HOME="${targetDir}/cargo"
+
+rustup-init --no-modify-path -y -q
+
+. "$CARGO_HOME/env"
+
+cd ${srcDir}
+cargo build --release
